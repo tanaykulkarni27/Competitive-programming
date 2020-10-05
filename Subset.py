da6528aed3c78bc360972b8dec40b58842d14272
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-10-05 01:01:12 -0700

    Update Subset.py

--- Subset.py
+++ Subset.py
@@ -1,20 +1,13 @@
-def add(subset,set):
-    complete = []
-    if len(subset) == len(set):
-        return complete
-    else:
-        complete.append(subset)
-        return complete
 def get_subsets(set):
+    subs = []
     N = len(set)
     for i in range((1<<N)):
         subset = []
         for j in range(N):
             if 1<<j & i:
                 subset.append(set[j])
-        ans = add(subset,set)        
-    if len(ans)>=1<<N
-        return ans
+        subs.append(subset)
+    return subs
 set = input('Enter Elements of set : ').split(sep=' ')
 subset = get_subsets(set)
 print(subset)
