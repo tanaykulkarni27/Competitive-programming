e428ebde14397fbeb54be9c40da1056f2e0ffae1
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-10-17 10:06:51 +0530

    Update Longest Arrithmatic.py

--- Longest Arrithmatic.py
+++ Longest Arrithmatic.py
@@ -1,26 +1,26 @@
 class case:
     def __init__(self,A):
-        self.A = A
-    def get_result(self):
-        counter = 1
-        lengths = []
-        for i in range(1,len(self.A)-1):
-          varA = self.A[i] - self.A[i-1]
-          VarB = self.A[i+1] - self.A[i]
-          if varA  == VarB:
-            counter = 1
-            for j in range(i-1,len(self.A)-1):
-                if self.A[j+1]-self.A[j] == varA:
-                    counter = counter+1
-            if counter>1:
-                lengths.append(counter)
-        if len(lengths)>=0:
-            return max(lengths)
-        else:
-            return 0
-Case = int(input())
-N = int(input())
-A = [int(i) for i in input().split()]
-if N == len(A):
-    ans = case(A).get_result()
-    print('Case #{}: {}'.format(Case,ans))
+        self.integers = A
+        self.Max_Length = 0
+        self.Old_Max_Length = 0
+    def get_solution(self):
+        for i in range(len(self.integers)-1):
+            j = i;temp = 1
+            common_difference = self.integers[i+1]-self.integers[i]
+            while j<len(self.integers)-1:
+                if self.integers[j+1]-self.integers[j]==common_difference:
+                    temp+=1
+                else:
+                    break
+                j+=1
+            self.Max_Length = temp
+            if self.Max_Length > self.Old_Max_Length:
+                self.Old_Max_Length=self.Max_Length
+        return self.Old_Max_Length
+T = int(input())
+for i in range(1, T + 1):
+    N = int(input())
+    integers = [int(i) for i in input().split()]
+    obj = case(integers)
+    ans = obj.get_solution()
+    print('Case #{}: {}'.format(i,ans))
