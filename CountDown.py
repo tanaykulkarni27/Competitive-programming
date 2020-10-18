78d65a6db2c4a990b91d698feca21e461194cb89
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-10-18 11:53:18 +0530

    Update CountDown.py

--- CountDown.py
+++ CountDown.py
@@ -3,23 +3,19 @@ class case:
         self.K = K
         self.A = A
         self.chk = False
-        self.decreasing_counter = 0
     def solve(self):
-        count_downs = 0;i=0
-        while i < len(self.A):
-            if self.K-self.decreasing_counter == self.A[i] and i == len(self.A)-1 and self.chk:
-                count_downs+=1
-            if self.A[i] == self.K-self.decreasing_counter:
-                self.decreasing_counter+=1
-                self.chk = True
-            elif self.A[i] != self.K-self.decreasing_counter and self.K and self.decreasing_counter>1:
-                count_downs+=1
-                self.decreasing_counter*=0
-                self.chk = False
-                if self.A[i] == self.K:
-                    i-=1
+        i=0;count = 0;ans = 0
+        while i <len(self.A):
+            if self.A[i] == self.K:
+                count = 1
+                # decreasing counter < K and index+1 < len(Array)  
+                while (count<self.K and i+1<len(self.A) and self.A[i+1] == self.K-count):
+                    i+=1
+                    count+=1
+                if count == self.K :
+                    ans+=1
             i+=1
-        return count_downs
+        return ans
 T = int(input())
 for i in range(1,T+1):
     N,K = [int(i) for i in input().split()]
