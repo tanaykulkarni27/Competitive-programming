8dffba1f23f844ea4893b6bf6dc5dfc52bdbbbb7
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-10-29 11:13:42 +0530

    Update Book Reading.py

--- Book Reading.py
+++ Book Reading.py
@@ -1,33 +1,22 @@
-class case :
-    def __init__(self,No_of_pages,list_of_torn_pages,list_of_multiples):
-        self.No_of_pages = No_of_pages
-        self.list_of_torn_pages = sorted(list_of_torn_pages)
-        self.list_of_multiples = list_of_multiples
-        self.count = 0
-    def get_result(self):
-        for j in list_of_multiples:
-            for i in range(1,self.No_of_pages+1):
-                if i%j==0:
-                    length = len(self.list_of_torn_pages)-1
-                    result = self.search(self.list_of_torn_pages,0,length,i)
-                    if result:
-                        self.count+=1
-        return self.count
-    def search(self, array,l,r,  target):
-        mid = (l+r)//2
-        if l>r:
-            return True
-        if array[mid] ==target or array[0]==target or array[r]==target:
-            return False
-        elif array[mid]>target:
-            return self.search(array,l,mid-1,target)
-        elif array[mid]<target:
-            return self.search(array,mid+1,r,target)
+class case:
+    def __init__(self,reader_m,pages,torned):
+        self.torned = torned
+        self.reader_m = reader_m
+        self.pages = pages
+    def solve(self):
+        ans = 0;X=1
+        for reader in self.reader_m:
+            X=1
+            while reader*X <= self.pages :
+                if reader*X not in self.torned:
+                    ans+=1
+                X+=1
+        return ans
 T = int(input())
 for i in range(1,T+1):
-    No_of_pages,No_of_torn_pages,No_of_reader = [int(i) for i in input().split()]
-    list_of_torn_pages = [int(i) for i in input().split()]
-    list_of_multiples = [int(i) for i in input().split()]
-    obj = case(No_of_pages,list_of_torn_pages,list_of_multiples)
-    ans = obj.get_result()
+    pages,torn_pages,reader = [int(i) for i in input().split()]
+    torned_page_number = [int(i) for i in input().split()]
+    reader_multiples = [int(i) for i in input().split()]
+    obj = case(reader_multiples,pages,torned_page_number)
+    ans = obj.solve()
     print('Case #{}: {}'.format(i,ans))
