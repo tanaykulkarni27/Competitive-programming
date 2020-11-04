4941b71240ae0205975cd0b65033c4effc7088e2
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-11-04 16:17:42 +0530

    Update knap.py

--- knap.py
+++ knap.py
@@ -13,6 +13,16 @@ assume that dp[n][len(w)] is the maximum number of items
 
 
 '''
+'''
+
+1
+3 50
+60 100 120
+10 20 30
+
+answer = 220
+
+'''
 def read_str():
     return str(input())
 def read_int():
@@ -21,35 +31,28 @@ def read_str_array():
     return [str(i) for i in input().split()]
 def read_int_array():
     return [int(i) for i in input().split()]
-def dp_using_bottom_up(index):   # this is bottom up approach Time it takes n * w(   capacity of knapsack    )
-    dp= [[0]*len(weights)]*len(weights)
-    for i in range(len(weights)):
-        for j in range(len(weights)):
-            if i ==0 or j==0:
-                dp[i][j] = 0
-            elif weights[i]<=capacity:
-                dp[i][j] =  max(dp[i-1][j],prices[i]+dp[i-1][j-weights[i]])
+def KS_bottom_up(W,val,wt,index):
+    K = [[0 for x in range(W + 1)] for y in range(n + 1)]
+    for i in range(n+1):
+        for j in range(W+1):
+            if i == 0 or j == 0 :
+                K[i][j] = 0
+            elif wt[i-1]<=j:
+                K[i][j] = max(val[i-1]+K[i-1][j-wt[i-1]],K[i-1][j])
             else:
-                dp[i][j] = dp[i-1][j]
-    return dp[i][j]
-def dp_using_recursion(c,w,v,index,memo):   # this is recursive approach Time it takes n^2
-   if index == 0 or c == 0:
-       return 0
-   if w[index]>c:
-       return dp_r(c,w,v,index-1)
-   else:
-       tmp = v[index] + dp_r(c - w[index], w, v, index - 1,memo)
-       tmpb = dp_r(c,w,v,index-1,memo)
-       a = max(tmp, tmpb)
-       memo[len(w,),c] = a
-       return a
-def solve():
-    result = dp_r(capacity,weights,prices,len(weights)-1,{})
-    return result
-T = int(input())
+                K[i][j] = K[i-1][j]
+    return K[n][W]
+def KS_recursion(W,val,wt,index):
+    if W == 0 or index>=len(wt):return 0
+    elif wt[index]>W:
+        return KS_recursion(W,val,wt,index+1)
+    else:
+        res = max(val[index]+KS_recursion(W-wt[index],val,wt,index+1),KS_recursion(W,val,wt,index+1))
+        return res
+T= read_int()
 for _ in range(1,T+1):
-    capacity = read_int()
+    n,K = read_int_array()
+    values = read_int_array()
     weights = read_int_array()
-    prices = read_int_array()
-    ans = solve()
+    ans = KS_recursion(K,values,weights,0)
     print('Case #{}: {}'.format(_,ans))
