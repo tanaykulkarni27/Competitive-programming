d22be3df7b355ff8cb6698be7b0c0134960543b1
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-11-09 10:31:02 +0530

    Update Huge Numbers.py

--- Huge Numbers.py
+++ Huge Numbers.py
@@ -1,30 +1,32 @@
-class case:
-    def __init__(self,A,N,P):
-        self.A = A
-        self.N = N
-        self.P = P
-        self.tmp = 1
-        self.memo = {}
-    def find_ans(self,A,N):
-        if N > self.N:
-            return self.tmp%self.P
-        self.tmp = self.pow(A,N,self.memo)
-        return self.find_ans(self.tmp,N+1)
-    def pow(self,A,n,memo):
-        if n == 0:
-            return 1
-        elif memo.get(n)!=None:
-            return memo.get(n)
-        else:
-            a = A* self.pow(A,n-1,memo)
-            memo[n]=a
-            return a
-    def get_result(self):
-        final = self.find_ans(self.A,1)
-        return final
-T = int(input())
-for i in range(1,T+1):
-    A,N,P = [int(j) for j in input().split()]
-    obj = case(A,N,P)
-    ans = obj.get_result()
-    print('Case #{}: {}'.format(i,ans))
+import sys
+sys.setrecursionlimit(int(100000))
+def read_str():
+    return str(input())
+def read_int():
+    return int(input())
+def read_str_array():
+    return [str(i) for i in input().split()]
+def read_int_array():
+    return [int(i) for i in input().split()]
+# temp = 0
+
+def pow(n,p,memo = {}):
+    if p == 1:
+        return n
+    if (n,p) in memo:
+        return memo.get((n,p))
+    else:
+        ans = n*pow(n,p-1,memo)
+        memo[(n,p)] = ans
+        return ans
+def solve():
+    ans = A%P
+    for i in range(2,N+1):
+        ans = pow(ans,i)%P
+    return ans
+T = read_int()
+for _ in range(1,T+1):
+    A,N,P = read_int_array()
+    ans = solve()
+    print('Case #{}:'.format(_),end=' ')
+    print(ans)
