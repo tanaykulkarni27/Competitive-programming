7099e5cf046bffdaa94b2a82ab00d9c15179e6cb
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-11-09 21:12:08 +0530

    Update Huge Numbers.py

--- Huge Numbers.py
+++ Huge Numbers.py
@@ -1,5 +1,5 @@
-import sys
-sys.setrecursionlimit(int(1000000))
+# import sys
+# sys.setrecursionlimit(int(100000))
 def read_str():
     return str(input())
 def read_int():
@@ -10,19 +10,23 @@ def read_int_array():
     return [int(i) for i in input().split()]
 # temp = 0
 
-def pow(n,p,memo = {}):
+def pow(n,p,mod,memo):
+    if (n,p) in memo: return memo.get((n,p))
     if p == 0:
         return 1
-    half_pow = pow(n,p//2)
-    half_sqr = (half_pow*half_pow)
+    half_pow = pow(n,p//2,mod,memo)
+    half_sqr = (half_pow*half_pow)%mod
     if p%2 == 0:
+        memo[(n,p)] = half_sqr
         return half_sqr
     else:
-        return half_sqr*n
+        memo[(n, p)] = (half_sqr*n)%mod
+        return (half_sqr*n)%mod
 def solve():
     ans = A%P
+    memo = {}
     for i in range(2,N+1):
-        ans = pow(ans,i)%P
+        ans = pow(ans,i,P,memo)
     return ans
 T = read_int()
 for _ in range(1,T+1):
