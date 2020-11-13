cc666d240d88ab5f5dde42202fcc2f596d20fe12
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-11-13 13:37:08 +0530

    Update Unival_tree.py

--- Unival_tree.py
+++ Unival_tree.py
@@ -1,26 +1,38 @@
+def read_str():
+    return str(input())
+def read_int():
+    return int(input())
+def read_str_array():
+    return [str(i) for i in input().split()]
+def read_int_array():
+    return [int(i) for i in input().split()]
 class tree:
     def __init__(self,data):
         self.data = data
         self.left = None
         self.right = None
-    def count_unival(self,root):
+    def count(self,root):
         if root == None:
-           return 0
-        total = self.count_unival(root.left)+self.count_unival(root.right)
-        if self.chk_unival(root):
-            total+=1
-        return total
-    def chk_unival(self,root):
+            return 0
+        else:
+            total = self.count(root.right) +self.count(root.left)
+            if self.chk_node(root):
+                total+=1
+            return total
+    def chk_node(self,root):
         if root == None:
+            return False
+        elif root.left == None and root.right == None:
             return True
-        if root.left!= None and root.left.data == root.data and root.right!= None and root.right.data == root.data:
+        elif root.left != None and root.left.data == root.data  and root.right != None and root.right.data == root.data:
             return True
-
+        else:
+            return False
 if __name__ == '__main__':
-    root = tree(2)
+    root = tree(3)
     root.left = tree(2)
-    root.right = tree(2)
+    root.right = tree(3)
     root.right.right = tree(2)
     root.right.right.left = tree(2)
     root.right.right.right = tree(2)
-    print(root.count_unival(root))
+    print(root.count(root))
