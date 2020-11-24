da460fccc448ec04a31627eda8aa741ec47abcb8
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2020-11-24 17:05:04 +0530

    Update Cakes.py

--- Cakes.py
+++ Cakes.py
@@ -1,30 +1,35 @@
-class case:
-    def __init__(self,M):
-        self.N = N
-        self.cakes_need_to_eat = 0
-        self.i =1
-
-    def square_root(self,N, i):
-        if N == 1:
-            return 1
-        if i * i == N:
-            return i
-        elif i * i > N:
-            return i - 1
-        else:
-            return self.square_root(N, i + 1)
-    def recursive(self,N,cakes_eaten):
-        if N <= 0:
-            return cakes_eaten
-        a = self.square_root(N,1)
-        cakes_eaten+=1
-        return self.recursive(N-a*a,cakes_eaten)
-    def get_result(self):
-        ans = self.recursive(self.N,self.cakes_need_to_eat)
-        return ans
+from math import sqrt
+def read_int():
+    return int(input())
+def read_string():
+    return str(input())
+def read_int_array():
+    return [int(i) for i in input().split()]
+def read_str_array():
+    return [str(i) for i in input().split()]
+def chk(N): # Checking N is Perfect Square of Not
+    i = int(sqrt(N))
+    if N == int(i*i) :
+        return True
+    else:
+        return False
+def s(N):
+    return N*N
+def solve(N):
+    if chk(N):return 1
+    else:
+        for i in range(1,int(sqrt(N))+1):
+            for j in range(i,int(sqrt(N))+1):
+                if (i*i+j*j == N):
+                    return 2
+        for i in range(1,int(sqrt(N))+1):
+            for j in range(i,int(sqrt(N))+1):
+                for k in range(j,int(sqrt(N))+1):
+                    if (s(i)+s(j)+s(k) == N):
+                        return 3
+        return 4
 T = int(input())
-for test_case in range(1,T+1):
-    N = int(input())
-    obj = case(N)
-    ans = obj.get_result()
-    print('Case #{}: {}'.format(test_case,ans))
+for _ in range(1,T+1):
+    N = read_int()
+    ans = solve(N)
+    print('Case #{}: {}'.format(_,ans))
