dc71819a20280d012215f133ec008bedf09e6692
Tanay <71624876+tanaykulkarni27@users.noreply.github.com>
2021-03-19 19:10:53 +0530

    Update trie.cpp

--- trie.cpp
+++ trie.cpp
@@ -1,132 +1,81 @@
+#include<stdio.h>
 #include<iostream>
 #include<bits/stdc++.h>
+#include<map>
+#include <vector> 
+#include<string>
+using namespace std;
 #define FOR(start,end) for(int i=start;i<end;i++)
 #define lli long long int 
 #define read(a) cin>>a;
-#define readr(b,n) for(int i=0;i<n;i++){cin>>b[i];}
-#define pass ;
+#define readr(b,n) for(lli i=0;i<n;i++){cin>>b[i];}
 #define printarr(arr,n) FOR(0,n){cout<<arr[i]<<" "; }cout<<endl;
-using namespace std;
-//reverse
-void reverse(lli arr[],lli n){
-	lli mid = (lli)((n-1)/2);
-	lli last = n-1;
-	FOR(0,mid+1){
-	lli temp = arr[last-i];
-	arr[last-i] = arr[i];
-	arr[i] = temp;	
-	}
-}
-// deletion
-int del(int arr[],int n,int index){
-	if(n==0)
-	return 0;
-	FOR(index,n){
-		arr[i] = arr[i+1];
-	}
-	return n-1;
-}
-int remove(int arr[],int n,int element){
-	int temp = 0;
-	int index;
-	FOR(0,n){
-	if(arr[i] == element){
-		temp = i;
-		break;
-	}	
-	}
-	if(temp == 0)
-	return n;
-	else
-	del(arr,n,temp);	
-}
-
-// A^N
-lli power(lli a,lli n){
-	if(n==0)
-	return 1;
-	lli half_power = power(a,(lli)n/2);
-	if(n%2==0)
-	return half_power*half_power;
-	else
-	return half_power*half_power*a;
-}
-// linear search
-bool linear_search(long long int arr[],long long int n,long long int find){
-	FOR(0,n){
-		if(arr[i] == find)
-		return 0;
-	}
-	return 1;
-}
-// binary search
-bool binary_search(int arr[],int l,int r,int find){
-int mid = l+(r-1)/2;
-cout<<mid<<"\n";  
-if(arr[l] == find || arr[r] == find || arr[mid] == find)
-return 0;
-if(l>r)
-	return 1;
-if(arr[l]<find)
-return binary_search(arr,mid+1,r,find);
-return binary_search(arr,l,mid-1,find);
-}
-// insertion
-lli append(lli arr[],lli n,lli element){
-	arr[n] = element;
-	return n+1;
-}
-// Start Area to write solution
-struct node{
-	struct node* arr[26];
-	bool isend ;
+const long int MOD = 1000000007;
+struct lnode{
+	bool is_end;
+	struct lnode* chars[26];
 };
-struct node* getnode(){
-	struct node* child = new node;
-	child->isend = false;
-	for(int i=0;i<26;i++)
-	child->arr[i] = NULL;
-	return child;
-}
-void insert(struct node* root ,string key){
-	struct node* temp = root;
-	for(int i=0;i<key.length();i++){
-		int index = (int)key[i]-97;
-		if(!temp->arr[index])
-			temp->arr[index] = getnode();
-		temp = temp->arr[index];
+struct lnode* new_node(){
+		struct lnode* nw = new lnode();
+		for (int i = 0; i < 26; ++i)
+				{
+					nw->chars[i] = NULL;
+				}		
+		nw->is_end = false;
+		return nw;
 	}
-	temp->isend = true;
+void del(struct lnode* root,string key){
+	struct lnode* tmp = root;
+	for (int i = 0; i < key.length() && tmp!= NULL; ++i)
+	{
+		int index = key[i]-'a';
+		tmp = tmp->chars[index];
+	}
+	if(tmp!=NULL)
+		tmp->is_end = false;
+	return;
 }
-bool search(struct node* root,string st){
-	struct node* t = root;
-	int pnt = 0;
-	for(pnt=0;pnt<st.length();pnt++){
-		int index = (int)st[pnt]-'a';	
-		if(!t->arr[index])
+void insert(struct lnode* tmp,string data){
+		//struct lnode* tmp = root;
+		for(auto i : data){
+			int index = i-'a';
+			if(!tmp->chars[index])
+				tmp->chars[index] = new_node();
+			tmp = tmp->chars[index];
+		}
+		tmp->is_end = true;
+}	
+bool search(struct lnode* root,string key){
+	struct lnode* tmp = root;
+	for(auto i : key){
+		int index = i-'a';
+		if(tmp->chars[index] == NULL)
 			return false;
-		else
-		t = t->arr[index];
+		tmp = tmp->chars[index];
 	}
-	return (t!=NULL && t->isend);
+	return tmp!=NULL && tmp->is_end ;
 }
-// End Area to write solution
-int main(){
-	string a[5];
-	string sear[5];
-	readr(a,5);
-	readr(sear,5);
-	struct node* obj = getnode();
-	for(auto v : a){
-		insert(obj,v);
+void dfs(struct lnode* root,string s){
+	if(root->is_end == true)
+		cout<<s<<" ";
+	for (int i = 0; i < 26; ++i)
+	{ 
+		if(root->chars[i]!=NULL)
+			dfs(root->chars[i],s+(char)(i+'a'));
 	}
-	for(auto v : sear){
-		cout<<search(obj,v)<<endl;
+}
+void solve(){
+	struct lnode* root = new_node();
+	string s[5] = {"tanay","abhinav","yashodhan","prashant","anupama"};
+	for(auto i : s)
+		insert(root,i);
 	
-	}
+	string res = search(root,"tanay")? "WORD FOUND ":"WORD NOT FOUND"; 
+	cout<<res;
+
+	//dfs(root,"");
+}
+int main(){
+	solve();
 	return 0;
 }
-/*
-tanay tanhaji reshma sanika dhananjay
-taay tanhaji reshma sanika dhananjay
-*/
